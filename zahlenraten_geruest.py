5f817514d1d1625a51fe2c6eff9c4d6fda5c5e15
tayler <Tayler.Meienberg@gmx.de>
2026-06-22 13:24:52 +0200

    fix : clean up

--- zahlenraten_geruest.py
+++ zahlenraten_geruest.py
@@ -3,7 +3,7 @@ import random
 
 
 def geheime_zahl_erzeuge():
-    # TODO: Zufallszahl erzeugen und mit "return" zurückgeben
+
     return random.randint(1, 100)
 
 
@@ -16,7 +16,6 @@ def spieler_fragen():
 
 
 def tipp_geben(tipp, geheim):
-    # TODO: Vergleiche tipp und geheim und gib den passenden Text aus (print)
     if tipp < geheim:
         print("die zahl ist größer")
     elif tipp > geheim:
@@ -29,12 +28,7 @@ def spiel_starten():
     print("Ich denke an eine Zahl zwischen 1 und 100.")
     geheim = geheime_zahl_erzeuge()
     versuch = 0
-
-    # TODO 1: geheime Zahl erzeugen und in einer Variablen speichern
-
-    # TODO 2: einen Zähler für die Versuche bei 0 starten
     while True :
-
         tipp = spieler_fragen()
         versuche = versuch + 1
         versuch = versuche
@@ -46,19 +40,10 @@ def spiel_starten():
     print("geschafft! du hast " + str(versuche) + " versuche gebraucht")
 
 
-    # TODO 3: Schleife bauen, die wiederholt, bis richtig geraten wurde
-    #         (Tipp: while-Schleife)
-    #         In der Schleife:
-    #           - Spieler fragen
-    #           - Versuche-Zähler um 1 erhöhen
-    #           - Tipp geben
-    #           - prüfen, ob der Tipp gleich der geheimen Zahl ist -> dann aufhören
 
-    # TODO 4: am Ende ausgeben, wie viele Versuche gebraucht wurden
     pass
 
 
-# Diese Zeile sorgt dafür, dass das Spiel startet,
-# wenn man die Datei ausführt. Lass sie einfach so stehen.
+
 if __name__ == "__main__":
     spiel_starten()
